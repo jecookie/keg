a945fba8a0daf0815ea5ef504b0f5622e687df27
Jesse <jessecooke@gmail.com>
2017-10-18 05:10:33 +0000

    lots of bug fixes

--- Kegomatic/kegomatic.py
+++ Kegomatic/kegomatic.py
@@ -4,11 +4,19 @@ import time
 import math
 import logging
 import RPi.GPIO as GPIO
-import datetime
+import time
 from flowmeter import *
-from adabot import *
-from subprocess import call
+from os import system
 #from seekrits import *
+
+import signal
+import sys
+def signal_handler(signal, frame):
+        print('You pressed Ctrl+C!')
+        GPIO.cleanup()
+        sys.exit(0)
+signal.signal(signal.SIGINT, signal_handler)
+
 
 LITERS_IN_A_KEG = 18
 
@@ -17,7 +25,6 @@ GPIO.setmode(GPIO.BCM) # use real GPIO numbering
 GPIO.setup(23,GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(24,GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-view_mode = 'normal'
 
 # set up the flow meters
 fm = FlowMeter('imp', ["beer"])
@@ -37,25 +44,24 @@ def doAClick2(channel):
 
 def updatePage(f1, f2):
 	#Find and replace OZ1 with last pour
-	call("cp /home/pi/kegbot/beer_www/rightBeerOunces.html /home/pi/kegbot/beer_www/rightBeerOuncesNow.html")
-	call("sed -i 's/OZ1/This Pour      :" + f1.getFormattedThisPour() + "/' /home/pi/kegbot/beer_www/rightBeerOuncesNow.html")
-	call("sed -i 's/OZ2/Before that    :" + f1.getFormattedLastPour() + "/' /home/pi/kegbot/beer_www/rightBeerOuncesNow.html")
-	call("sed -i 's/OZ3/And Before That:" + f1.getFormattedLasterPour() + "/' /home/pi/kegbot/beer_www/rightBeerOuncesNow.html")
+	system("cp /home/pi/kegbot/beer_www/rightBeerOunces.html /home/pi/kegbot/beer_www/rightBeerOuncesNow.html")
+	system("sed -i 's/OZ1/This Pour      :" + f1.getFormattedThisPour() + "/' /home/pi/kegbot/beer_www/rightBeerOuncesNow.html")
+	system("sed -i 's/OZ2/Before that    :" + f1.getFormattedLastPour() + "/' /home/pi/kegbot/beer_www/rightBeerOuncesNow.html")
+	system("sed -i 's/OZ3/And Before That:" + f1.getFormattedLasterPour() + "/' /home/pi/kegbot/beer_www/rightBeerOuncesNow.html")
 	
 	#This calculation is in Liters
-	percent_left = round(1 - (f1.totalPour / LITERS_IN_A_KEG) * 100)	
-	call("sed -i 's/RM1/" + str(percent_left) + "/' /home/pi/kegbot/beer_www/rightBeerOuncesNow.html")
+	percent_left = round((1 - (f1.totalPour / LITERS_IN_A_KEG)) * 100,0)	
+	system("sed -i 's/RM1/" + str(percent_left) + "/' /home/pi/kegbot/beer_www/rightBeerOuncesNow.html")
 	
 def updateSave(f1, f2):	
-	with open("keg_save_file.txt","w") at fout:
-		fout.writelines(f1.thisPour+"\n")
-		fout.writelines(f1.lastPour+"\n")
-		fout.writelines(f1.lasterPour+"\n")
-		fout.writelines(f1.totalPour+"\n")
-		file.close()
+	with open("keg_save_file.txt","w") as fout:
+		fout.writelines(str(f1.thisPour)+"\n")
+		fout.writelines(str(f1.lastPour)+"\n")
+		fout.writelines(str(f1.lasterPour)+"\n")
+		fout.writelines(str(f1.totalPour)+"\n")
+		fout.close()
 		
-		st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
-		print "File written at "+st+"\n"
+                print time.ctime() + ": This pour "+f1.getFormattedThisPour() +"\n"
 	
     
 GPIO.add_event_detect(23, GPIO.RISING, callback=doAClick, bouncetime=20) # Beer, on Pin 23
@@ -63,12 +69,22 @@ GPIO.add_event_detect(24, GPIO.RISING, callback=doAClick2, bouncetime=20) # Selt
 
 #TODO: Load saves from a file
 
+#Test the functions now
+# Update the webpage
+updatePage(fm, fm2)
+#Update the savefile
+updateSave(fm,fm2)
+fm.clear()
+GPIO.cleanup()
+sys.exit(0)
+
+
+
 # main loop
 while True:
 
   currentTime = int(time.time() * FlowMeter.MS_IN_A_SECOND)
  
-  view_mode = 'normal'
     
   # reset flow meter after each pour (2 secs of inactivity)
   if (fm.thisPour >= 0.05 and currentTime - fm.lastClick > 2000):
@@ -77,6 +93,9 @@ while True:
 		#Update the savefile
 		updateSave(fm,fm2)
 		fm.clear()
+
+#  print str(fm.thisPour*fm.OUNCES_IN_A_LITER) + "\n"
+  time.sleep(1)
 		
     
   #if (fm2.thisPour <= 0.23 and currentTime - fm2.lastClick > 2000):
